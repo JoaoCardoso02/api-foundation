6df4bb8f71d08cb9f208e298531d63435bf94308
João Cardoso <joaolucas.cardoso02@gmail.com>
2022-08-05 07:44:03 -0300

    feat: add example service interface

--- src/domain/example/services/ExampleService.ts
+++ src/domain/example/services/ExampleService.ts
@@ -1,30 +1,33 @@
 import ExampleRepository from '@domain/example/infrastructure/ExampleRepository'
-import { ICreateExample } from '@domain/example/types/ICreateExample'
+import Example from '@domain/example/entities/Example'
 
-export default class ExampleService {
+import { ICreateExample } from '@domain/example/types/ICreateExample'
+import { IExampleService } from '@domain/example/types/IExampleService'
+
+export default class ExampleService implements IExampleService {
 	private readonly exampleRepository: ExampleRepository
 
 	constructor() {
 		this.exampleRepository = new ExampleRepository()
 	}
 
-	findAll() {
+	findAll(): Example[] {
 		return this.exampleRepository.getAll()
 	}
 
-	findOne(id: number) {
+	findOne(id: number): Example | null {
 		return this.exampleRepository.getOne(id)
 	}
 
-	create(example: ICreateExample) {
+	create(example: ICreateExample): Example {
 		return this.exampleRepository.create(example)
 	}
 
-	update(id: number, example: ICreateExample) {
+	update(id: number, example: ICreateExample): Example {
 		return this.exampleRepository.update(id, example)
 	}
 
-	delete(id: number) {
+	delete(id: number): boolean {
 		return this.exampleRepository.delete(id)
 	}
 }
