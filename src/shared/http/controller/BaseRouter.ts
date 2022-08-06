8f3238893d8072670eb3f7c60dae05c0af87130b
João Cardoso <joaolucas.cardoso02@gmail.com>
2022-08-06 17:11:41 -0300

    feat: add BaseRouter to use in router files

---
+++ src/shared/http/controller/BaseRouter.ts
@@ -0,0 +1,30 @@
+import { Router } from 'express'
+import BaseController from './BaseController'
+
+export default class BaseRouter {
+	private router: Router
+
+	constructor(router: Router) {
+		this.router = router
+	}
+
+	post(path: string, handler: BaseController) {
+		this.router.post(path, handler.execute)
+	}
+
+	get(path: string, handler: BaseController) {
+		this.router.get(path, handler.execute)
+	}
+
+	patch(path: string, handler: BaseController) {
+		this.router.patch(path, handler.execute)
+	}
+
+	put(path: string, handler: BaseController) {
+		this.router.put(path, handler.execute)
+	}
+
+	delete(path: string, handler: BaseController) {
+		this.router.delete(path, handler.execute)
+	}
+}
