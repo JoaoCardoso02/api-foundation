601890c1240ffc3fb15aeb8f78d45380eceaed82
João Cardoso <joaolucas.cardoso02@gmail.com>
2022-08-08 13:52:03 -0300

    feat: add mongodb dependencies

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
 	collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
 	coverageDirectory: '<rootDir>/../coverage',
 	testEnvironment: 'node',
+	preset: '@shelf/jest-mongodb',
 	rootDir: 'src',
 	transform: {
 		'.+\\.ts$': 'ts-jest',
